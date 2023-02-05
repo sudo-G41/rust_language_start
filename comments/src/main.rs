3c7d631d367decc3a74e2c97a49be254215b69e6
Sucheol.J <usa7372@gmail.com>
2023-02-05 12:00:42 +0000

    [FEAT] comments 주석... 코드...? user : sudo-G41 day  : 2023.02.05.

---
+++ comments/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Hello, world.
+    // So we’re doing something complicated here, long enough that we need
+    // multiple lines of comments to do it! Whew! Hopefully, this comment will
+    // explain what’s going on.
+    //주석을 달아보자
+    println!("Hello, world!");
+}
